6616c540258e76702e652f73e6e627b6b3b68c6b
Alexis Orphant <alexisorphant@gmail.com>
2022-09-22 23:09:26 -0300

    gh pages

--- src/components/Category.js
+++ src/components/Category.js
@@ -7,7 +7,7 @@ function Category() {
 
     return (
         <div className="max-w-[1640px] m-auto px-4 py-12">
-            <h1 className="text-orange-600 font-bold text-4xl text-center">Platos Más Valorados</h1>
+            <h1 className="text-orange-600 font-bold text-4xl text-center ">Platos Más Valorados</h1>
             {/* categorias */}
             <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
                 {categories.map((item, index) => (
