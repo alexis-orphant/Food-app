7ff4fa9b4eff6eba626bb15c13766442516513a6
Alexis Orphant <alexisorphant@gmail.com>
2022-09-22 23:22:09 -0300

    cc

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -21,7 +21,7 @@ function NavBar() {
                     <AiOutlineMenu size={30} />
                 </div>
                 <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
-                    Best <span className="font-bold">Eats</span>
+                    Best  <span className="font-bold">Eats</span>
                 </h1>
                 <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px">
                     <p className="bg-black text-white rounded-full p-2">
